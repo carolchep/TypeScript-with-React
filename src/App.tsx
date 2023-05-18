7478cf8e71087344d6f7864ecdb2ba8a500131da
carolchep <carolchep@gmail.com>
2023-05-18 23:03:22 +0300

    object-props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import { Greet } from './components/Greet';
-
+import { Person } from './components/Person'
 function App() {
+  const personName ={
+    first : "Bruce",
+    last:"Wayne"
+  }
   return (
     <div className="App">
-     <Greet name ="carol" messageCount={10} isLoggedIn={false}/>
+      <Greet name="carol" messageCount={10} isLoggedIn={false} />
+      <Person name={personName}/>
     </div>
   );
 }
---
+++ src/components/Person.tsx
@@ -0,0 +1,13 @@
+type PersonProps = {
+    name: {
+        first: string
+        last: string
+    }
+}
+export const Person = (props: PersonProps) => {
+    return (
+        <div>
+            {props.name.last}and {props.name.first}
+        </div>
+    )
+}
