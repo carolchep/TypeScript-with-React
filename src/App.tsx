ed663b7e7033a416048763d47fce34fe2a2003d7
carolchep <carolchep@gmail.com>
2023-05-19 23:27:32 +0300

    array-prop=types

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,33 @@ import React from 'react';
 import './App.css';
 import { Greet } from './components/Greet';
 import { Person } from './components/Person'
+import { PersonList } from './components/PersonList';
+
 function App() {
   const personName ={
     first : "Bruce",
     last:"Wayne"
   }
+  const personList=[
+    {
+      first :"caro",
+      last:"sam"
+    },
+    {
+      first :"memo",
+      last:"chep"
+    },
+    {
+      first :"chela",
+      last:"kaku"
+    }
+   
+  ]
   return (
     <div className="App">
       <Greet name="carol" messageCount={10} isLoggedIn={false} />
       <Person name={personName}/>
+      <PersonList names={personList}/>
     </div>
   );
 }
