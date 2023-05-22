4e733bf10f39821c032dfcee448dc56923ef7f5d
carolchep <carolchep@gmail.com>
2023-05-23 00:25:40 +0300

    bug-fix

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,10 +3,10 @@ type ButtonProps={
 }
 
 export const Button =(props:ButtonProps)=>{
-    // return(
-    //     <button onClick={props.handleClick}>
-    //     </button>
-    // )
+    return(
+        <button onClick={props.handleClick}>
+        </button>
+    )
 }
 //Accept a click event as a prop and pass it on to the html button elemnt 
 // handleClick :()=>void used in a situation where handleclick function isn't returning anythingit 
