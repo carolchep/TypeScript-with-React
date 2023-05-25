afbae40d2d7711ffecd9b14565423d19f6f73bd2
carolchep <carolchep@gmail.com>
2023-05-25 22:50:20 +0300

    type-inference

---
+++ src/components/state/LoggedIn.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+export const LoggedIn = () => {
+    const [isLoggedIn, setisLoggedIn] = useState(false)
+
+    const handleLogin = () => {
+        setisLoggedIn(true)
+
+    }
+    const handleLogout = () => {
+        setisLoggedIn(false)
+    }
+
+    return (
+        <div>
+            <button onClick={handleLogin}>Login</button>
+            <button onClick={handleLogout}>login</button>
+            <div>User is {isLoggedIn ?'logged IN':'Logged out'}</div>
+
+
+        </div>
+    )
+}
