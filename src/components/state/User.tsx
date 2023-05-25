c75a38906710ce53dc88ee7322d4ac39d032cda3
carolchep <carolchep@gmail.com>
2023-05-25 23:29:24 +0300

    Type-assertion

--- src/components/state/User.tsx
+++ src/components/state/User.tsx
@@ -4,7 +4,10 @@ type AuthUser={
     email :string
 }
 export const User = () => {
-const [user,setUser]=useState <AuthUser |null >(null)
+    //telling typescript value can be null or as in Authuser
+// const [user,setUser]=useState <AuthUser |null >(null)
+const [user,setUser]=useState <AuthUser >({}as AuthUser)
+
     const handleLogin = () => {
         setUser({
             name :'Carol',
@@ -12,16 +15,16 @@ const [user,setUser]=useState <AuthUser |null >(null)
         }
         )
     }
-    const handleLogout = () => {
-        setUser(null)
+    // const handleLogout = () => {
+    //     setUser(null)
 
-    }
+    // }
 
     return (
         <div>
             <button onClick={handleLogin}>Login</button>
-            <button onClick={handleLogout}>login</button>
-            <h1>user name is{user?.name}</h1>
+            {/* <button onClick={handleLogout}>login</button> */}
+            <h1>user name is{user.name}</h1>
             <h2>user name is </h2>
 
         </div>
